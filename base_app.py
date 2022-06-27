780e42c3b005f352fd8f1ada56423bb506147a84
Vmeleka <104976684+Vmeleka@users.noreply.github.com>
2022-06-28 00:04:44 +0300

    monday modification
    modification to include the three models and sample pictures and a video

--- base_app.py
+++ base_app.py
@@ -49,6 +49,11 @@ def main():
 	options = ["Prediction", "Information", "About the company"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
+    
+    
+    
+    
+    
 	# Building out the "Information" page
 	if selection == "Information":
 		st.title("Project Information")
@@ -62,22 +67,47 @@ def main():
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
             
             
+            
+            
+            
 
 	# Building out the predication page
 	if selection == "Prediction":
-		st.title("Tweet Classifier")
+		st.title("Tweet Classifier")     
 		st.subheader("Climate change tweet classification")
 		st.info("Prediction with ZF3 Machine learning Model")
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter your Text","Type Here")
+		tweet_text = st.text_area("Enter your Text","Type Here")        
+        
+        # you can create multiple pages this way
+		options = ["lm", "naive", "mcv"]
+		selection = st.sidebar.selectbox("Select Model", options)
 
 		if st.button("Click to Classify"):
-			# Transforming user input with vectorizer
+		# Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("forest_model.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+		# Load your .pkl file with the model of your choice + make predictions
+		# Try loading in multiple models to give the user a choice
+            
+			if selection == "lm":  
+				predictor = joblib.load(open(os.path.join("forest_model.pkl"),"rb"))
+				prediction = predictor.predict(vect_text)
+                    
+			if selection == "naive":
+				predictor = joblib.load(open(os.path.join("forest_model.pkl"),"rb"))
+				prediction = predictor.predict(vect_text)
+                    
+			if selection == "mcv":  
+				predictor = joblib.load(open(os.path.join("forest_model.pkl"),"rb"))
+				prediction = predictor.predict(vect_text)                   
+                   
+                    
+#				predictor = joblib.load(open(os.path.join("forest_model.pkl"),"rb"))
+#				prediction = predictor.predict(vect_text)
+
+
+
+
 
 	# Building our company's profile page
 	if selection == "About the company":
@@ -85,11 +115,42 @@ def main():
 		st.subheader("Welcome to ZF3 company")
 		st.info("The company was founded in June 2022 with the following pioneers")
 		# You can read a markdown file from supporting resources folder
+        
+        
+
+        
+        #image doc code format
+
+		from PIL import Image
+		image1 = Image.open('abcf.jpg')        
+		victor_image = Image.open('victor.jpg')
+		Francis_image = Image.open('abcf.jpg')        
+		Abigael_image = Image.open('abcf.jpg')
+		Michael_image = Image.open('abcf.jpg')
+		Raheeemat_image = Image.open('abcf.jpg')
+		Harmony_image = Image.open('abcf.jpg') 
+        
+		st.image(image1, caption='Sunrise by the mountains', width=350)
+		st.image(victor_image, caption='Model Expert', width=350) 
+        
+        #documentation for images
+        #st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+        
+        
+
+
+
 		st.markdown("Francis Ikegwu,Abigael Kinini, Michael Mamah, Raheeemat Adetunji,Victor Meleka, Harmony Odumuko") 
 
 		st.subheader("More information")
 		if st.checkbox('Show contact information'): # data is hidden if box is unchecked
-			st.info("francisikegwu@yahoo.com, kininiabigael@gmail.com,mamahchidike@gmail.com, icontola@gmail.com, vicmeleka@gmail.com, nibotics@gmail.com") # will write the df to the page            
+			st.info("francisikegwu@yahoo.com, kininiabigael@gmail.com,mamahchidike@gmail.com, icontola@gmail.com, vicmeleka@gmail.com, nibotics@gmail.com") # will write the df to the page  
+        # video format /doc
+		video_file = open('sdw.mp4', 'rb')
+		video_bytes = video_file.read()
+
+		st.video(video_bytes)            
+            
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
